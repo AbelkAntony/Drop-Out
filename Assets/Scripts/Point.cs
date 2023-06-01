c8d44553d22bc92ff5d85f6a09093b780b7d0cad
ABEL K ANTONY <abelkantony98@gmail.com>
2023-06-01 20:34:07 +0530

    update

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Point : MonoBehaviour
 {
-
+    public int point = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Bird")
         {
+            AddPoint();
             Destroy(collision.gameObject);
         }
+    }
+
+    private void AddPoint()
+    {
+        point++;
     }
 }
